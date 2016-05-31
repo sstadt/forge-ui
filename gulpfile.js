ea81368e1d481cd826b8898108ace037467edf57
scott.stadt@lunch2.com <scott.stadt@lynch2.com>
2016-05-31 16:33:04 -0500

    fixed compilation of foundation with the foundation component mixins

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp       = require('gulp');
+var sass       = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function() {
   gulp.src('src/sass/base.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass({
-      includePaths: ['src/sass', 'node_modules/foundation-sites/scss']
+      includePaths: ['./node_modules/foundation-sites/scss'],
+      outputStyle: 'compressed'
     }).on('error', sass.logError))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist/css'));
 });
 
