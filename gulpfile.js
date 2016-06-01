a2282591602ea064685364f3792d5987996849ef
scott.stadt@lunch2.com <scott.stadt@lynch2.com>
2016-06-01 09:11:05 -0500

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,26 @@
 var gulp       = require('gulp');
 var sass       = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var livereload = require('gulp-livereload');
+
 
 gulp.task('sass', function() {
-  gulp.src('src/sass/base.scss')
+  return gulp.src('src/sass/myui.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({
       includePaths: ['./node_modules/foundation-sites/scss'],
       outputStyle: 'compressed'
     }).on('error', sass.logError))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('dist/css'))
+    .pipe(livereload());
 });
 
-gulp.task('default', ['sass'], function() {
+
+gulp.task('watch', ['default'], function() {
+	livereload.listen({ start: true });
   gulp.watch(['scss/**/*.scss'], ['sass']);
 });
+
+
+gulp.task('default', ['sass']);
