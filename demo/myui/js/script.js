2523259971285a68a0ab5bc36eb28942caa5e774
Scott Stadt <scott.stadt@gmail.com>
2016-06-05 11:59:03 -0500

    refactored alert example for clarity

--- demo/myui/js/script.js
+++ demo/myui/js/script.js
@@ -4,11 +4,20 @@ Vue.config.debug = true;
 new Vue({
   el: '#alert_demo',
   data: {
-    alertMessage: ''
+    alertMessage: '',
+    selectedMessageType: 'message',
+    messageTypes: [
+      { value: 'message', label: 'vm.$refs.myAlert.message()' },
+      { value: 'primary', label: 'vm.$refs.myAlert.primary()' },
+      { value: 'secondary', label: 'vm.$refs.myAlert.secondary()' },
+      { value: 'success', label: 'vm.$refs.myAlert.success()' },
+      { value: 'warning', label: 'vm.$refs.myAlert.warning()' },
+      { value: 'alert', label: 'vm.$refs.myAlert.alert()' },
+    ]
   },
   methods: {
     newMessage(type) {
-      this.$refs.myAlert[type](this.alertMessage);
+      this.$refs.myAlert[this.selectedMessageType](this.alertMessage);
     }
   }
 });
