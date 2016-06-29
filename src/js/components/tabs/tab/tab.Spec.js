f67f26b2b07a8687f042e59129d2a3f1eaba29a0
scott.stadt@lunch2.com <scott.stadt@gmail.com>
2016-06-29 15:18:37 -0500

    full coverage !!!!

--- src/js/components/tabs/tab/tab.Spec.js
+++ src/js/components/tabs/tab/tab.Spec.js
@@ -25,13 +25,9 @@ describe('The tab component', function () {
       data = component.data();
     });
 
-    // it('should have an initial index value of 0', function () {
-    //   expect(data.index).toEqual(0);
-    // });
-    //
-    // it('should have an initial show value of false', function () {
-    //   expect(data.show).toEqual(false);
-    // });
+    it('should have an initial active value of false', function () {
+      expect(data.active).toEqual(false);
+    });
   });
 
   describe('props', function () {
@@ -39,25 +35,56 @@ describe('The tab component', function () {
       expect(component.props).toEqual(jasmine.any(Object));
     });
 
-    // describe('heading', function () {
-    //   it('should exist', function () {
-    //     expect(component.props.heading).toEqual(jasmine.any(Object));
-    //   });
-    //
-    //   it('should be a string', function () {
-    //     expect(component.props.heading.type).toEqual(String);
-    //   });
-    // });
+    describe('heading', function () {
+      it('should be a string', function () {
+        expect(component.props.heading.type).toEqual(String);
+      });
+
+      it('should be required', function () {
+        expect(component.props.heading.required).toEqual(true);
+      });
+    });
   });
 
-  describe('methods', function () {
+  describe('ready lifecycle method', function () {
     var componentInstance;
 
     beforeEach(function () {
       componentInstance = new Vue(component);
+      componentInstance.heading = 'foo';
+      spyOn(componentInstance, '$dispatch');
+      componentInstance.$mount('body');
     });
 
-    // need to find a way to compile this to test references to parent
+    it('should dispatch a TAB_COMPONENT_TAB_CREATED with the tab heading', function () {
+      expect(componentInstance.$dispatch).toHaveBeenCalledWith('TAB_COMPONENT_TAB_CREATED', 'foo');
+    });
+  });
+
+  describe('events', function () {
+    var componentInstance;
+
+    beforeEach(function () {
+      componentInstance = new Vue(component);
+      componentInstance.heading = 'foo';
+    });
+
+    it('should be an object', function () {
+      expect(component.events).toEqual(jasmine.any(Object));
+    });
+
+    describe('TAB_COMPONENT_TAB_CLICKED', function () {
+      it('should activate if passed the name of the heading', function () {
+        componentInstance.$emit('TAB_COMPONENT_TAB_CLICKED', 'foo');
+        expect(componentInstance.active).toEqual(true);
+      });
+
+      it('should deactivate if not passed the name of the heading', function () {
+        componentInstance.active = true;
+        componentInstance.$emit('TAB_COMPONENT_TAB_CLICKED', 'bar');
+        expect(componentInstance.active).toEqual(false);
+      });
+    });
   });
 
 });
